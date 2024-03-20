a6a014a5ef8f62d07878817ba43dea995732c469
Ygor Garofalo <igorgarofalo@gmail.com>
2024-03-20 15:46:03 +0100

    adds analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ParallaxProvider } from "react-scroll-parallax";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ParallaxProvider>
     <App />
+    <Analytics />
   </ParallaxProvider>
 );
